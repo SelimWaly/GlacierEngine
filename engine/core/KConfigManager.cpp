8acfbc49e1d805eb6e83683860780b72ced19c3b
Selim Waly <selimwalyy@gmail.com>
2024-06-24 22:48:16 +0300

    Manage frames with KManager

---
+++ engine/core/KConfigManager.cpp
@@ -0,0 +1,277 @@
+﻿//  **************************************
+//  File:        KConfigManager.cpp
+//  ***************************************
+#include "KConfigManager.h"
+#include "renderingengine.h"
+#include "boost/lexical_cast.hpp"
+#include "FilePath.h"
+#include "KStringManager.h"
+
+#define MAX_LINE_SIZE	(512)
+
+using namespace std;
+using namespace boost;
+
+
+KConfigManager * g_config_m = NULL;
+KConfigManager* getConfigManager()
+{
+	if (g_config_m == NULL)
+	{
+		kn_string strPath = GetCurrentPath();
+		strPath += _T("config/nplan_config.xml");
+
+		g_config_m = new KConfigManager(strPath);
+	}
+	return g_config_m;
+}
+
+void releaseConfigManager()
+{
+
+	if (g_config_m)
+	{
+		g_config_m->saveConfig();
+	}
+	SAFE_DELETE(g_config_m);
+}
+
+KConfigManager::KConfigManager(kn_string strFilePath)
+{
+	m_oneLayerElement = NULL;
+	m_pXmldoc = NULL;
+	m_twoLayerElement = NULL;
+	loadConfigFromFile(strFilePath);
+
+#ifdef WIN32
+	TCHAR szPath[MAX_PATH];
+	GetCurrentDirectory( MAX_PATH, szPath);
+	m_app_path = szPath;
+	m_app_path+=_T("/");
+#endif
+}
+
+
+KConfigManager::~KConfigManager()
+{
+	if (m_pXmldoc)
+	{
+		delete m_pXmldoc;
+	}
+}
+
+kn_string KConfigManager::getAppPath()
+{
+	return m_app_path;
+}
+
+kn_bool KConfigManager::loadConfigFromFile (kn_string strFilePath)
+{
+	//XML
+	m_file = ns2s( strFilePath );
+	m_pXmldoc = new TiXmlDocument(m_file.c_str());
+	if (m_pXmldoc->LoadFile())
+	{
+		TiXmlElement* pRootElement = m_pXmldoc->RootElement();
+		if ( pRootElement )
+		{
+			// 
+			m_oneLayerElement = pRootElement->FirstChildElement("onelayer");
+			//  ()
+			m_twoLayerElement = pRootElement->FirstChildElement("twolayer");
+		}
+		else
+		{
+			return false;
+		}
+	}
+	else
+	{
+		//,XML.
+		m_pXmldoc->Clear();
+        knDeleteFile(strFilePath.c_str());
+
+		//
+		TiXmlElement* pRootElement = new TiXmlElement("GLACIERConfig");
+		m_oneLayerElement = new TiXmlElement("onelayer");
+		m_twoLayerElement = new TiXmlElement("twolayer");
+		pRootElement->LinkEndChild(m_oneLayerElement);
+		pRootElement->LinkEndChild(m_twoLayerElement);
+		TiXmlElement* pHistorys = new TiXmlElement("historys");
+		m_twoLayerElement->LinkEndChild(pHistorys);
+		m_pXmldoc->LinkEndChild(pRootElement);
+
+		//
+		TiXmlElement* plang = new TiXmlElement("language");
+		TiXmlText* text = new TiXmlText("0");
+		plang->LinkEndChild(text);
+		m_oneLayerElement->LinkEndChild(plang);
+		return false;
+	}
+
+	m_language = getIntConfig(_T("language"));
+	return true;
+}
+
+kn_int KConfigManager::getLanguage()
+{
+	return m_language;
+}
+
+kn_string KConfigManager::getLanguageImagePath()
+{
+	kn_string strPath = _T("");
+	switch (getConfigManager()->getLanguage() )
+	{
+	case LAN_CN:
+		strPath += _T("cn_image/");
+		break;
+	case LAN_EN:
+		strPath  += _T("en_image/");
+		break;
+	case LAN_JP:
+		strPath  += _T("jp_iamge/");
+		break;
+	default:
+		strPath  += _T("cn_image/");
+		break;
+	}
+	return strPath;
+}
+
+//config
+void KConfigManager::saveConfig()
+{
+	if (m_pXmldoc)
+	{
+#ifdef WIN32
+		SetFileAttributes(s2ns(m_file).c_str(),0);
+#endif
+		m_pXmldoc->SaveFile(m_file.c_str());
+	}
+}
+
+/**
+* ini
+*/
+kn_string KConfigManager::getConfig(const kn_string str)
+{
+	kn_string strResult = _T("");
+
+	if (m_oneLayerElement)
+	{
+		TiXmlElement* childElement = m_oneLayerElement->FirstChildElement(ns2s(boost::to_lower_copy(str)).c_str());
+		
+		if (childElement)
+		{
+			strResult = GetTextString(childElement);
+		}
+		else
+		{
+			kn_string str_v = _T("0");
+			setConfig(str, strResult);
+		}
+	}
+	return strResult;
+}
+/**
+* ini
+*/
+kn_int KConfigManager::getIntConfig(const kn_string str)
+{
+	kn_int iResult = 0;
+	
+
+	if (m_oneLayerElement)
+	{
+		TiXmlElement* childElement = m_oneLayerElement->FirstChildElement(ns2s(boost::to_lower_copy(str)).c_str());
+
+		if (childElement)
+		{
+			iResult = atoi(childElement->GetText());
+		}
+		else
+		{
+			kn_string str_v = _T("0");
+			setConfig(str, str_v);
+		}
+	}
+	return iResult;
+}
+
+kn_bool KConfigManager::setConfig(const kn_string strValue,const kn_int v)
+{
+	return setConfig(strValue, lexical_cast<kn_string>(v ) );
+}
+
+/**
+* ini
+*/
+kn_bool KConfigManager::setConfig(const kn_string strValue,const kn_string str)
+{
+	if (m_oneLayerElement)
+	{
+		TiXmlElement* childElement = m_oneLayerElement->FirstChildElement(ns2s(boost::to_lower_copy(strValue)).c_str());
+
+		if (childElement)
+		{
+			TiXmlText* text = new TiXmlText(ns2s(boost::to_lower_copy(str)).c_str());
+			childElement->Clear();
+			childElement->LinkEndChild(text);
+		}
+		else
+		{
+			childElement = new TiXmlElement(ns2s(boost::to_lower_copy(strValue)).c_str());
+			TiXmlText* text = new TiXmlText(ns2s(boost::to_lower_copy(str)).c_str());
+			childElement->Clear();
+			childElement->LinkEndChild(text);
+
+			m_oneLayerElement->LinkEndChild(childElement);
+
+		}
+	}
+ 	return true;
+}
+
+ void KConfigManager::GetVectorConfig(const kn_string& str, vector<kn_string>& vHistory )
+ {
+	 if (m_twoLayerElement)
+	 {
+		 TiXmlElement* childElement = m_twoLayerElement->FirstChildElement(ns2s(boost::to_lower_copy(str)).c_str());
+		 if (childElement)
+		 {
+
+			 childElement = childElement->FirstChildElement("history");
+			 while (childElement)
+			 {
+				 vHistory.push_back(GetTextString(childElement));
+				 childElement = childElement->NextSiblingElement();
+			 }
+		 }
+	 }
+ }
+
+ kn_bool KConfigManager::addHistoryConfig( const kn_string& str, const kn_string& strHistory )
+ {
+	 if (m_twoLayerElement)
+	 {
+		 TiXmlElement* childElement = m_twoLayerElement->FirstChildElement(ns2s(boost::to_lower_copy(str)).c_str());
+
+		 if (childElement)
+		 {
+			 TiXmlElement* reElement = new TiXmlElement ( "history" );
+
+#ifdef _UNICODE 
+			 TiXmlText *text = new TiXmlText( UnicodeToUTF8(strHistory).c_str() );
+#else
+			 TiXmlText *text = new TiXmlText( strHistory.c_str() );
+#endif
+			 reElement->Clear();
+			 reElement->LinkEndChild(text);
+
+			 childElement->LinkEndChild( reElement );
+
+		 }
+	 }
+	 return true;
+ }
---
+++ engine/core/KGalleryView.cpp
@@ -0,0 +1,185 @@
+﻿//  **************************************
+//  File:        KGalleryView.cpp
+//  ***************************************
+#include "KGalleryView.h"
+#include "KImageDrawable.h"
+#include "DataSync.h"
+#include "KScreen.h"
+//////////////////////////////////////////////////////////////////////////
+//	KGalleryItemView
+
+
+KGalleryItemView::KGalleryItemView()
+{
+	m_e_viewtype = KVIEW_GALLERY_ITEM;
+
+}
+
+KGalleryItemView::~KGalleryItemView()
+{
+
+}
+
+void KGalleryItemView::SetFileName(kn_string&  str)
+{
+	m_strFileName = str;
+}
+
+kn_string KGalleryItemView::GetFileName()
+{
+	return m_strFileName;
+}
+
+
+
+//////////////////////////////////////////////////////////////////////////
+//	KGalleryView
+
+KGalleryView::KGalleryView()
+{
+	m_e_viewtype = KVIEW_GALLERY;
+
+	m_i_thum_width = 200;
+	m_i_thum_height = 140;
+
+ 
+
+	m_i_horizontal_interval = 20;
+	m_i_vertical_interval = 20;
+}
+
+KGalleryView::~KGalleryView()
+{
+
+
+}
+
+
+void KGalleryView::setGridStyle(kn_int w, kn_int h, kn_int hi, kn_int vi)
+{
+	 m_i_thum_width = w;
+	 m_i_thum_height = h;
+	 m_i_horizontal_interval = hi;
+	 m_i_vertical_interval = vi;
+
+
+}
+
+ // drawable
+//void KGalleryView::bindDrawables(vector<KDrawable_PTR>& vDrawables)
+//{
+//
+//}
+
+ // 
+kn_bool KGalleryView::bindFilelist (vector<kn_string>& vFlieList)
+{
+	m_p_thum_groupview = KViewGroup_PTR( new KViewGroup );
+	m_p_thum_groupview->Create(0, 0, m_rect.width(), m_rect.height());
+
+	// 
+	kn_int col = m_rect.width() / (m_i_thum_width + m_i_horizontal_interval);
+	kn_int row = m_rect.height() / (m_i_thum_height + m_i_vertical_interval);
+
+	kn_uint iMaxInPage = col * row; 
+
+	for(kn_uint i = 0; i < vFlieList.size() && i < iMaxInPage; i++)
+	{ 
+		IRESurface* pSurface = RESurfaceFactory::CreateRESurface();
+		kn_bool bOK = pSurface->InitializeThumbnailFromFile(vFlieList[i], m_i_thum_width, m_i_thum_height, RE_FIT_WIDTH, RE_ColorBLACK);
+		if(!bOK)
+		{
+			ASSERT(0);
+			continue;
+		}
+
+		KImageDrawable_PTR imagedrawable = KImageDrawable_PTR(new KImageDrawable (pSurface));
+
+		KGalleryItemView_PTR pView = KGalleryItemView_PTR(new KGalleryItemView);
+
+		int x = (i % col) * (m_i_thum_width + m_i_horizontal_interval);
+		int y = (i / col) * (m_i_thum_height + m_i_vertical_interval);
+
+		pView->Create(x, y, m_i_thum_width, m_i_thum_height);
+		pView->setBKDrawable(imagedrawable);
+
+		//pView->setBKGImage(vFlieList[i]);
+
+		// 
+		pView->m_clicked_signal.connect(this, &KGalleryView::onItemClick);  
+		pView->SetFileName(vFlieList[i]);
+		m_p_thum_groupview->AddView(pView);
+ 
+	}
+
+
+	AddView(m_p_thum_groupview);
+
+	return true;
+
+}
+
+void KGalleryView::onItemClick(KView_PTR pView)
+{
+	KGalleryItemView_PTR pItemView = VIEW_PTR(KGalleryItemView)(pView->shared_from_this() );
+
+	MyTraceA ("onItemClick pItemView str = %S", pItemView->GetFileName().c_str());
+
+	pItemView->setViewFocus();
+
+	// 
+	//pItemView->setBKGImage(pItemView->GetFileName());
+	// kn_bool bEnlarge = (m_rect.width() > 400);
+
+   kn_bool bEnlarge = false;
+
+	if(bEnlarge)
+	{
+		RERect rect = pItemView->GetBoundRect();
+
+		float factor = rect.width() / rect.height();
+		rect.outset(factor * 200 , 200);
+
+		CPropertyRect * pPropertyRect = new CPropertyRect (pItemView, rect);
+
+		CGLACIERAnimation* pGLACIERAnimation = new CGLACIERAnimation (1000, 0, 0);	
+		CPropertyAnimationData* pAnimationData = new CPropertyAnimationData(pPropertyRect, KEasingCurve::OutCirc);
+		pGLACIERAnimation->addAnimationData(pAnimationData);
+
+		pGLACIERAnimation->setLoopType(CGLACIERAnimation::LOOP_PINGPONG);
+		pGLACIERAnimation->setLoopTimes(100);
+
+		m_ani_thread.clearAnimation();
+		m_ani_thread.addAnimation(pGLACIERAnimation);
+		m_ani_thread.Start();
+	}
+	else
+	{
+		RERect rect = pItemView->GetBoundRect();
+		pItemView->SetClip(rect);
+
+		RERect rectTarget = rect;
+		rectTarget.inset(300, 200);
+
+
+
+		CPropertyClip * pPropertyClip = new CPropertyClip (pItemView, rectTarget);
+		CGLACIERAnimation* pGLACIERAnimation = new CGLACIERAnimation (2000, 0, 0);	
+		CPropertyAnimationData* pAnimationData = new CPropertyAnimationData(pPropertyClip );
+		pGLACIERAnimation->addAnimationData(pAnimationData);
+
+		pGLACIERAnimation->setLoopType(CGLACIERAnimation::LOOP_PINGPONG);
+		pGLACIERAnimation->setLoopTimes(10000);
+
+		m_ani_thread.clearAnimation();
+		m_ani_thread.addAnimation(pGLACIERAnimation);
+		m_ani_thread.Start();
+
+	}
+
+
+
+
+
+	 
+}
