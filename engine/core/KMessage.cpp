399178967c9c41269b305f56a193bc607d38f192
Selim Waly <selimwalyy@gmail.com>
2024-06-24 22:51:07 +0300

    Verify logs as messages

---
+++ engine/core/KMessage.cpp
@@ -0,0 +1,72 @@
+﻿//  **************************************
+//  File:        KMessage.cpp
+//  ***************************************
+
+#include "KMessage.h"
+#include "DataSync.h"
+
+
+
+// 
+void KMsgQueue::AddUserMessage(KMessage* pMsg)
+{
+    writeLock lock(m_mutex);
+    m_dqUserMsg.push_back(pMsg);
+
+}
+
+// 
+KMessage* KMsgQueue::GetUserMessage()
+{
+	//	
+	if(m_dqUserMsg.empty())
+	{
+		return false;
+	}
+
+
+
+    //KAutoMutex amLock(m_mutex);
+
+    writeLock lock(m_mutex);
+
+    KMessage* pMsg = NULL;
+
+	if(!m_dqUserMsg.empty())
+	{
+        pMsg = m_dqUserMsg.front();
+        m_dqUserMsg.pop_front();
+
+	}
+
+    return pMsg;
+
+}
+ 
+// 
+KMsgQueue g_UserMsgQueue;
+
+
+//extern kn_hwnd g_hWndNavi;
+kn_dword SendKMessage(KMessage* pMsg)
+{
+    return PostKMessage(pMsg);
+
+
+}
+
+kn_dword PostKMessage(KMessage* pMsg)
+{
+
+    g_UserMsgQueue.AddUserMessage(pMsg);
+
+	return 0;
+}
+
+
+KMessage* GetKMessage()
+{
+
+    return g_UserMsgQueue.GetUserMessage();
+}
+ 
